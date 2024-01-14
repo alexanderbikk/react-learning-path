4292feb051091721ed0ac4c10a79dfc3460cde54
Oleksandr Bikkuzhyn <oleksandr.bikkuzhyn@nordkap.se>
2024-01-14 22:59:53 +0200

    configure redux store

--- intermediate-react/redux/src/AdoptedPetContex.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const AdoptedPetContex = createContext();
-
-export default AdoptedPetContex;
--- intermediate-react/redux/src/App.jsx
+++ intermediate-react/redux/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./store";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-import AdoptedPetContex from "./AdoptedPetContex";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,11 +16,10 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AdoptedPetContex.Provider value={adoptedPet}>
+        <Provider store={store}>
           <header>
             <Link to="/">Adopt me</Link>
           </header>
@@ -28,7 +27,7 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
-        </AdoptedPetContex.Provider>
+        </Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
