5f487669333daa155ed40da4fac70b78b493653b
Oleksandr Bikkuzhyn <oleksandr.bikkuzhyn@nordkap.se>
2024-01-14 23:08:59 +0200

    configure reducer and action

--- intermediate-react/redux/src/store.js
+++ intermediate-react/redux/src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import adoptedPet from './adoptedPetSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: { adoptedPet },
 });
 
 export default store;
